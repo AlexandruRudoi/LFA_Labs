b63d4f6b20b8f96ef31878b58342275679113c3b
Alexander Rudoi <rudoi.alexandru25@gmail.com>
2025-04-06 21:51:35 +0300

    add grammar definition

---
+++ Lab_5/Lab_5.Domain/Grammar.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab_5.Domain;
+
+public class Grammar
+{
+    public HashSet<Symbol> NonTerminals { get; set; } = new();
+    public HashSet<Symbol> Terminals { get; set; } = new();
+    public List<Production> Productions { get; set; } = new();
+    public Symbol StartSymbol { get; set; }
+
+    public Grammar(Symbol startSymbol)
+    {
+        StartSymbol = startSymbol;
+    }
+
+    public void AddProduction(Symbol left, IEnumerable<Symbol> right)
+    {
+        Productions.Add(new Production(left, right));
+        NonTerminals.Add(left);
+        foreach (var sym in right)
+        {
+            if (sym.IsTerminal) Terminals.Add(sym);
+            else NonTerminals.Add(sym);
+        }
+    }
+}
---
+++ Lab_5/Lab_5.Domain/Production.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab_5.Domain;
+
+public class Production
+{
+    public Symbol Left { get; set; }
+    public List<Symbol> Right { get; set; }
+
+    public Production(Symbol left, IEnumerable<Symbol> right)
+    {
+        Left = left;
+        Right = right.ToList();
+    }
+
+    public override string ToString() => $"{Left} → {string.Join("", Right)}";
+}
