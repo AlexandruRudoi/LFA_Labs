6a3d5807f82f2aef38a9e480ce23df3585a65498
Alexander Rudoi <rudoi.alexandru25@gmail.com>
2025-04-06 21:52:23 +0300

    add grammar structure

---
+++ Lab_5.Domain/Symbol.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab_5.Domain;
+
+public class Symbol
+{
+    public string Name { get; set; }
+    public bool IsTerminal { get; set; }
+
+    public Symbol(string name, bool isTerminal)
+    {
+        Name = name;
+        IsTerminal = isTerminal;
+    }
+
+    public override string ToString() => Name;
+    public override bool Equals(object obj) => obj is Symbol s && s.Name == Name && s.IsTerminal == IsTerminal;
+    public override int GetHashCode() => (Name, IsTerminal).GetHashCode();
+}
